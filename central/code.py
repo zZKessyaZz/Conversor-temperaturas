a78caa4dd792aa95be6f41456a1e977c00c0a2b4
Késsya Cavalcanti <kessyacavalcanti2017@gmail.com>
2025-10-17 22:36:37 -0300

    Alteração das cores

--- central/code.py
+++ central/code.py
@@ -10,39 +10,39 @@ janela.resizable(False,False)
 
 
 #Frames
-frame_principal = tk.Frame(janela, bg = "#FBD3F9")
+frame_principal = tk.Frame(janela, bg = "#340B32")
 frame_principal.pack(fill = "both", expand = True)
 
 #########
-frame_inicial = tk.Frame(frame_principal, bg = "#FBD3F9")
+frame_inicial = tk.Frame(frame_principal, bg = "#340B32")
 frame_inicial.pack(pady=(50,30))
 
-label = tk.Label(frame_inicial, text="Temperatura inicial:",bg="#FBD3F9",font = ("Comic Sans MS", 10, "bold"))
+label = tk.Label(frame_inicial, text="Temperatura inicial:",fg = "#FFFFFF", bg="#340B32",font = ("Comic Sans MS", 12))
 label.pack(side="left", padx = 8)
 temp_inicial = tk.Entry(frame_inicial, justify='center',width=15)
 temp_inicial.pack(side = "left")
 escala_inicial = tk.Spinbox(frame_inicial, justify= "center",width=15)
 escala_inicial.pack(side = "left", padx = 5)
-label = tk.Label(frame_inicial, text="Escala termométrica inicial",bg="#FBD3F9", font = ("Comic Sans MS", 10, "bold"))
+label = tk.Label(frame_inicial, text="Escala termométrica inicial",fg = "#FFFFFF", bg="#340B32", font = ("Comic Sans MS", 12))
 label.pack(side = "left")
 
 ########
-frame_abaixo = tk.Frame(frame_principal, bg="#FBD3F9")
+frame_abaixo = tk.Frame(frame_principal, bg="#340B32")
 frame_abaixo.pack()
 
-label1 = tk.Label(frame_abaixo, text="Escala termométrica desejada:", bg="#FBD3F9",font = ("Comic Sans MS", 10, "bold"))
+label1 = tk.Label(frame_abaixo, text="Escala termométrica desejada:", fg = "#FFFFFF",bg="#340B32",font = ("Comic Sans MS", 13))
 label1.pack(side="top")
 escala_desejada = tk.Spinbox(frame_abaixo, justify="center",width=15)
 escala_desejada.pack(side = "bottom", padx = 5, pady = 5)
 
 ########
-frame_botoes = tk.Frame(frame_principal, bg = "#FBD3F9")
+frame_botoes = tk.Frame(frame_principal, bg = "#340B32")
 frame_botoes.pack()
 
 btn1 = tk.Button(frame_botoes, width=10, height= 2, text="Converter", font = ("Comic Sans MS", 12), bg= "#FFFFFF")
 btn1.pack(pady = (50))
 
-label = tk.Label(frame_botoes, text="Temperatura convertida:",bg="#FBD3F9",font = ("Comic Sans MS", 10, "bold"))
+label = tk.Label(frame_botoes, text="Temperatura convertida:",fg = "#FFFFFF", bg="#340B32",font = ("Comic Sans MS", 13))
 label.pack(side = "left", pady = 8)
 temp_inicial = tk.Entry(frame_botoes, justify='center')
 temp_inicial.pack(side = "left", padx = 8)
